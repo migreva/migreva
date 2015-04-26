651259f3e24c38ceac7e5a59f6f646951926837e
migreva <mikevee313@gmail.com>
2015-04-25 21:16:36 -0400

    first commit: express, jade, gulp, postcss, etc

--- scripts/main-ck.js
+++
@@ -1,1 +0,0 @@
-require.config({paths:{blog:"blog"}});window.console||(console={log:function(){}});require(["index","nav","helpers","lwf","fetch","constants"],function(){fetch=new Fetch;fetch.init();var e=new Nav;e.init();helpers=new Helpers});
--- scripts/webFonts/_lwf-ck.js
+++
@@ -1,6 +0,0 @@
-/* 
-	Local Web Fonts (.js) 
-
-Used deal with the web fonts. Register callbacks, etc. 
-
-*/require([],function(){function e(e,t){e=="Amatic SC"&&t=="n4"}WebFontConfig={google:{families:["Amatic+SC::latin"]},loading:function(){},active:function(){},inactive:function(){},fontloading:function(t,n){e(t,n)},fontactive:function(e,t){},fontinactive:function(e,t){}};(function(){var e=document.createElement("script");e.src=("https:"==document.location.protocol?"https":"http")+"://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js";e.type="text/javascript";e.async="true";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})()});
