9636a051113144484b0bf2d0b0f9d537e9cd7605
Mike Varano <mikevee313@gmail.com>
2015-10-29 20:57:07 -0400

    workingg

--- src/client/migreva.js
+++ src/client/migreva.js
@@ -1,10 +1,11 @@
-import animations from './lib/animations';
+import * as animations from './lib/animations';
 
+let count = 0;
 for(let node of document.getElementsByClassName('contact-link')) {
 
     // delay each
     setTimeout(function() {
       node.className = node.className.replace('opacity-0', '');
       animations.bounceAnimations.contactAnimation.applyTo(node);
-    }, 1000 * (index + 1));
+    }, 1000 * (count++));
 }
