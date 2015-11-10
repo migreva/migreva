726aafabf0289e9535f6cd3b75e713ce2d036d81
Mike Varano <mikevee313@gmail.com>
2015-11-10 02:27:46 -0500

    very crude drawing technique

--- src/client/skyline.js
+++ src/client/skyline.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import xr from 'xr';
+import assign from 'object-assign';
+
+let commandRegex = /([a-zA-Z]{1}(?:\s*-?\d+(?:[\s\,]-?\d+\s+)?))|$/g;
+console.log(commandRegex.global);
 
 export default class Skyline extends React.Component {
   constructor(props) {
     super(props);
-
   }
 
   pathStyle() {
@@ -18,8 +21,7 @@ export default class Skyline extends React.Component {
   renderElement = (element, index) => {
     let paths = [];
     for (let path of element.paths) {
-      paths.push(<path d={ path } style={ this.pathStyle() }/>)
-
+      paths.push(<PathElement d={ path } style={ this.pathStyle() } key={ `element-${index}-path-${paths.length}` }/>)
     }
 
     let circles = [];
@@ -41,6 +43,63 @@ export default class Skyline extends React.Component {
         { this.props.svgElements.map(this.renderElement) }
       </svg>
     )
+  }
+}
+
+class PathElement extends React.Component {
+  static defaultProps = {
+    style: {
+      stroke: 'black',
+      fill: 'white'
+    }
+  }
+  constructor(props) {
+    super(props);
+
+    this.state = assign({}, this.compilePoints(this.props.d));
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+
+      this.drawPoint();
+    }, 500);
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+
+      this.drawPoint();
+    }, 500);
+  }
+
+  compilePoints(d) {
+    let splits = d.split(commandRegex);
+    let commands = [];
+
+    for (let split of splits) {
+      if (!split.trim()) continue;
+      commands.push(split);
+    }
+
+    return {
+      commands,
+      appliedCommands: []
+    };
+  }
+
+  drawPoint = () => {
+    if (this.state.commands.length == this.state.appliedCommands.length) return;
+
+    let commands = this.state.commands;
+    let appliedCommands = this.state.appliedCommands;
+    appliedCommands.push(commands[appliedCommands.length]);
+
+    this.setState({ commands, appliedCommands })
+  }
+
+  render() {
+    return (<path d={ this.state.appliedCommands.join(' ') } style={ this.props.style }/>)
   }
 }
 
